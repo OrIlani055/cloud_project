bf20681d3dedb019b1f47a8292f44e9dd4ded9f7
Or Ilani <ori@safecharge.com>
2020-01-14 12:36:16 +0200

    udpate server

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const route = require('./routes/api')
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("welcome");
+});
+
+app.use("/api", route);
+
+app.listen(port, () => {
+    console.log("running on port" + port);
+});
